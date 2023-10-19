de7cbd85c54f61861a472fe65372a7a11206bfa8
Will <105895841+WillYangZzz@users.noreply.github.com>
2023-10-20 11:53:49 +1300

    set up router for home page

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 import * as Path from 'node:path'
-// import * as URL from 'node:url'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
 
 import express from 'express'
 import hbs from 'express-handlebars'
+import puppyRouter from './routes.js'
+import fs from 'node:fs/promises'
+
+const dataPath = Path.join(__dirname, './data/data.json')
+
+export async function getData() {
+  const data = await fs.readFile(dataPath, 'utf-8')
+  const  spinData= JSON.parse(data)
+  return spinData
+}
 
 
 const server = express()
@@ -18,5 +31,10 @@ server.set('view engine', 'hbs')
 server.set('views', Path.resolve('server/views'))
 
 // Your routes/router(s) should go here
+server.get('/', async (req, res) =>{
+  const spinData = await getData()
+  res.render('home', spinData)
+})
+
 
 export default server
